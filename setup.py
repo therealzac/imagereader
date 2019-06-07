ddc9986188ba6be95d9fd9f0ba64e4fd9de8afc2
Zac Wickstrom <zacwickstrom@Zacs-MacBook-Pro.local>
2019-06-07 14:55:35 -0700

    readded setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ImageReader',
+    version='0.1dev',
+    packages=['ImageReader',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
